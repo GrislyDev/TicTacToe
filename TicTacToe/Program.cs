cce85c6241168e0176298611420b8d2cce803bb1
GrislyDev <officialviktorwolf@gmail.com>
2021-02-21 18:29:39 +0200

    Added basic code

--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -6,7 +6,45 @@ namespace TicTacToe
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			// Enter
+		}
+	}
+	class TicTacToe
+	{
+		// Initialization of variables
+		const char SIGN_X = 'x';
+		const char SIGN_O = 'o';
+		const char SIGN_EMPTY = '.';
+		char[,] table;
+		Random random;
+
+		TicTacToe() // Constructor
+		{
+			random = new Random();
+			table = new char[3,3];
+		}
+
+		void Game() // Game logic
+		{
+			InitTable(); // Create new table
+
+			while (true)
+			{
+				// Human turn
+				// Check pos
+				// AI turn
+				// Check pos
+			}
+		}
+
+		void PrintTable() // Display playing field
+		{
+			
+		}
+
+		void InitTable() // Initialization of table
+		{
+			
 		}
 	}
 }
